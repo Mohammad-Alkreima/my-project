e8295fe086a2f4cbd739467f53f04fca47ead916
fatimah-mse <fatimahmse83@gmail.com>
2025-07-26 23:17:20 +0300

    edit-hero-imgs

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,19 +1,20 @@
 import "./Cards.css"
 import Card from "../Card/Card"
-// import img from "../../../public/assets/img/location.svg"
-// import img-1 from "../../../public/assets/img/dollar-circle.svg"
+import img from "/assets/img/location.svg"
+import img1 from "/assets/img/dollar-circle.svg"
+import img2 from "/assets/img/dollar-circle.svg"
 const Cards = () => {
     return (
         <div className="cards">
-            <Card icon= "/assets/img/location.svg"
+            <Card icon= {img}
                 title="Location"
                 description="Ahmedabad, India"
             />
-            <Card icon="/assets/img/dollar-circle.svg"
+            <Card icon={img1}
                 title="Price"
                 description="$1000 - $10,000"
             />
-            <Card icon="/assets/img/house.svg"
+            <Card icon={img2}
                 title="Type of Property"
                 description="Apartment"
             />
